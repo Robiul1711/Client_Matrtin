dc9a05ffb2c01baa5001f327ed7bd6a55807d575
Robiul1711 <ashiq.web.dev@gmail.com>
2025-04-07 11:06:55 +0600

    style issue solved

--- app/layout.js
+++ app/layout.js
@@ -1,35 +1,36 @@
-import {Lexend as LexendFont, Open_Sans as OpenSansFont } from "next/font/google";
+import { Lexend as LexendFont, Open_Sans as OpenSansFont } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import StyledJsxRegistry from "./registry";
 
-// Initialize the fonts with different variable names
+// Initialize the fonts
 const Open_Sans = OpenSansFont({
-  variable: "--font-open-sans",  // Change the variable name
+  variable: "--font-open-sans",
   subsets: ["latin"],
 });
 
 const Lexend = LexendFont({
-  variable: "--font-lexend",  // Change the variable name
+  variable: "--font-lexend",
   subsets: ["latin"],
 });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${Open_Sans.variable} ${Lexend.variable}`}>
-        <div>
-          {/* Navbar */}
-          <Navbar />
+    <html lang="en" className={`${Open_Sans.variable} ${Lexend.variable}`}>
+      <body>
+        <StyledJsxRegistry>
+          <div>
+            {/* Navbar */}
+            <Navbar />
 
-          {/* Main content */}
-          <main>
-            {children}
-          </main>
+            {/* Main content */}
+            <main>{children}</main>
 
-          {/* Footer */}
-          <Footer />
-        </div>
+            {/* Footer */}
+            <Footer />
+          </div>
+        </StyledJsxRegistry>
       </body>
     </html>
   );
